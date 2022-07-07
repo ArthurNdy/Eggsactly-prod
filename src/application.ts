0f193a11deb835962d603fa1cc4e7f53b1ebb81b
ArthurNdy <65896591+ArthurNdy@users.noreply.github.com>
2022-07-07 12:40:29 +1000

    Update application.ts

--- src/application.ts
+++ src/application.ts
@@ -9,10 +9,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-app.use((_req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-});
-
 app.use(cors());
 // app.use(bodyParser);
 
